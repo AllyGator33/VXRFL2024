36e82b38012eb7942b5b15a695760eb1e2933e58
AllyGator33 <alyssa.campbell@lethbridgecollege.ca>
2024-10-15 10:39:48 -0600

    Made New Repo

---
+++ FL24VXR_Alyssa/Assets/VXR1180/1170_Scripts/Test2.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterName : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int badHealth = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
